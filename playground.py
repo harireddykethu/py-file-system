c69e0ca3fe03caa87d7f389a7d968f7b5e3a6e9c
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-20 17:13:08 +0530

    done

--- playground.py
+++ playground.py
@@ -1,14 +1,11 @@
-# Any I/O operation is exception prone
-# therefore, we ought to handle the errors and take
-# necessary corrective measures
+content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-# we emulate an erroneous situation by misspelt file name
 
 try:
-    file_object = open('poem.txt', 'rt', encoding='utf-8')
-    for line in file_object:
-        print(f'Reading: {line}')
+    file_object = open('sample.txt', 'wt', encoding='utf-8')
+    file_object.write(content)
     file_object.close()
+
 
 except FileNotFoundError as e:
     print('Oops, we could not find the file')
