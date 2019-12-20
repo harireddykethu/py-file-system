6686a61f289c91d0ba366ab5790e4acda5f94102
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-20 17:19:47 +0530

    done

--- playground.py
+++ playground.py
@@ -1,11 +1,8 @@
 content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 
-try:
-    file_object = open('sample.txt', 'wt', encoding='utf-8')
-    file_object.write(content)
-    file_object.close()
-
-
-except FileNotFoundError as e:
-    print('Oops, we could not find the file')
+with open('sample_1.txt', 'wt', encoding='utf-8') as file_reference:
+    file_reference.write(content)
+    file_reference.write(content)
+    file_reference.write(content)
+    file_reference.write(content)
